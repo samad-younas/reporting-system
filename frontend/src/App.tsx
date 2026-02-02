2cfc17b86e6a50eb5d28dd8915328883dda7190e
HannanShehzad <nauman33183@gmail.com>
2026-02-02 14:18:29 +0500

    make routes protected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
-import React, { lazy, Suspense } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React, { lazy, Suspense, useEffect } from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const UserManagement = lazy(() => import("./pages/ManageUser"));
 const Login = lazy(() => import("./pages/Login"));
@@ -7,21 +13,40 @@ const Profile = lazy(() => import("./pages/Profile"));
 const SSRSReports = lazy(() => import("./pages/SSRSReports"));
 import { LoadingSpinner } from "./components/public/LoadingSpinner";
 import { DashboardLayout } from "./components/layout/DashBoardLayout";
+import { useSelector } from "react-redux";
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 const App: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!token || !userdata) {
+      if (location.pathname !== "/login") {
+        navigate("/login");
+      }
+    } else {
+      if (location.pathname === "/login") {
+        navigate("/dashboard");
+      }
+    }
+  }, [location.pathname, navigate]);
+  const { userdata, token } = useSelector((state: any) => state.auth);
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<Navigate to="/dashboard" replace />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="ssrs-reports" element={<SSRSReports />} />
-          <Route path="user-management" element={<UserManagement />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        {!userdata || !token ? (
+          <Route path="/login" element={<Login />} />
+        ) : (
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="ssrs-reports" element={<SSRSReports />} />
+            <Route path="user-management" element={<UserManagement />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        )}
       </Routes>
     </Suspense>
   );
