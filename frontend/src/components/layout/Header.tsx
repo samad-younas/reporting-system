3572fd70e65e652b06ad8efd6d3088dc81e3eae2
Nauman <nauman33183@gmail.com>
2026-02-03 17:53:59 +0500

    login/logout functionality fixed

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -10,13 +10,30 @@ import {
   DropdownMenuSeparator,
 } from "../ui/dropdown-menu";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logOut } from "@/store/slices/authSlice";
+import { useSubmit } from "@/hooks/useSubmit";
+import { toast } from "react-toastify";
 
 interface HeaderProps {
   onMenuClick: () => void;
 }
 
 export const Header: React.FC<HeaderProps> = ({ onMenuClick }) => {
+  const { mutateAsync } = useSubmit({
+    method: "POST",
+    endpoint: "api/logout",
+    isAuth: true,
+  });
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const handleLogout = async () => {
+    await mutateAsync({});
+    dispatch(logOut());
+    toast.success("Logged out successfully");
+    navigate("/login");
+  };
+
   return (
     <header className="border-b px-3 md:px-4 lg:px-6 py-5 flex items-center justify-between sticky top-0 z-30 backdrop-blur-sm bg-card/95">
       <div className="flex items-center">
@@ -47,10 +64,7 @@ export const Header: React.FC<HeaderProps> = ({ onMenuClick }) => {
             View Profile
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem
-            variant="destructive"
-            onClick={() => navigate("/login")}
-          >
+          <DropdownMenuItem variant="destructive" onClick={handleLogout}>
             Logout
           </DropdownMenuItem>
         </DropdownMenuContent>
